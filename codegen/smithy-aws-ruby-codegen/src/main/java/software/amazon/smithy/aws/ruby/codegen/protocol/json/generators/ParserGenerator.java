a3bfce1d4578fb9a2185d203f9d2068abbed9e03
Matt Muller <mamuller@amazon.com>
2022-03-16 13:04:45 -0700

    Implement JSON rpc 1.1

--- codegen/smithy-aws-ruby-codegen/src/main/java/software/amazon/smithy/aws/ruby/codegen/protocol/json/generators/ParserGenerator.java
+++ codegen/smithy-aws-ruby-codegen/src/main/java/software/amazon/smithy/aws/ruby/codegen/protocol/json/generators/ParserGenerator.java
@@ -15,9 +15,27 @@
 
 package software.amazon.smithy.aws.ruby.codegen.protocol.json.generators;
 
-import software.amazon.smithy.model.shapes.*;
+import java.util.Optional;
+import java.util.stream.Stream;
+import software.amazon.smithy.model.shapes.BlobShape;
+import software.amazon.smithy.model.shapes.DoubleShape;
+import software.amazon.smithy.model.shapes.FloatShape;
+import software.amazon.smithy.model.shapes.ListShape;
+import software.amazon.smithy.model.shapes.MapShape;
+import software.amazon.smithy.model.shapes.MemberShape;
+import software.amazon.smithy.model.shapes.OperationShape;
+import software.amazon.smithy.model.shapes.SetShape;
+import software.amazon.smithy.model.shapes.Shape;
+import software.amazon.smithy.model.shapes.ShapeVisitor;
+import software.amazon.smithy.model.shapes.StructureShape;
+import software.amazon.smithy.model.shapes.TimestampShape;
+import software.amazon.smithy.model.shapes.UnionShape;
+import software.amazon.smithy.model.traits.JsonNameTrait;
+import software.amazon.smithy.model.traits.SparseTrait;
+import software.amazon.smithy.model.traits.TimestampFormatTrait;
 import software.amazon.smithy.ruby.codegen.GenerationContext;
 import software.amazon.smithy.ruby.codegen.generators.ParserGeneratorBase;
+import software.amazon.smithy.ruby.codegen.trait.NoSerializeTrait;
 
 public class ParserGenerator extends ParserGeneratorBase {
 
@@ -25,39 +43,261 @@ public class ParserGenerator extends ParserGeneratorBase {
         super(context);
     }
 
+    private void renderMemberParsers(Shape s) {
+        //remove members w/ http traits or marked NoSerialize
+        Stream<MemberShape> serializeMembers = s.members().stream()
+                .filter(NoSerializeTrait.excludeNoSerializeMembers());
+
+        serializeMembers.forEach((member) -> {
+            Shape target = model.expectShape(member.getTarget());
+            String dataName = symbolProvider.toMemberName(member);
+            String dataSetter = "data." + dataName + " = ";
+            String jsonName = member.getMemberName();
+
+            String valueGetter = "map['" + jsonName + "']";
+            target.accept(new MemberDeserializer(member, dataSetter, valueGetter, false));
+        });
+    }
+
     @Override
     protected void renderUnionParseMethod(UnionShape s) {
+        writer
+                .openBlock("def self.parse(map)")
+                .write("key, value = map.flatten")
+                .write("case key")
+                .call(() -> {
+                    s.members().forEach((member) -> {
+                        writer
+                                .write("when '$L'", unionMemberDataName(s, member))
+                                .indent()
+                                .call(() -> {
+                                    renderUnionMemberParser(s, member);
+                                })
+                                .write("Types::$L::$L.new(value) if value", symbolProvider.toSymbol(s).getName(),
+                                        symbolProvider.toMemberName(member))
+                                .dedent();
+                    });
+                })
+                .openBlock("else")
+                .write("Types::$L::Unknown.new({name: key, value: value})", s.getId().getName())
+                .closeBlock("end") // end of case
+                .closeBlock("end");
+    }
 
+    private String unionMemberDataName(UnionShape s, MemberShape member) {
+        String dataName = member.getMemberName();
+        String jsonName = dataName;
+        if (member.hasTrait(JsonNameTrait.class)) {
+            jsonName = member.getTrait(JsonNameTrait.class).get().getValue();
+        }
+        return jsonName;
+    }
+
+    private void renderUnionMemberParser(UnionShape s, MemberShape member) {
+        Shape target = model.expectShape(member.getTarget());
+        target.accept(new MemberDeserializer(member, "value = ",
+                "value", false));
     }
 
     @Override
     protected void renderMapParseMethod(MapShape s) {
-
+        writer
+                .openBlock("def self.parse(map)")
+                .write("data = {}")
+                .openBlock("map.map do |key, value|")
+                .call(() -> {
+                    Shape valueTarget = model.expectShape(s.getValue().getTarget());
+                    valueTarget
+                            .accept(new MemberDeserializer(s.getValue(),
+                                    "data[key] = ", "value",
+                                    !s.hasTrait(SparseTrait.class)));
+                })
+                .closeBlock("end")
+                .write("data")
+                .closeBlock("end");
     }
 
     @Override
     protected void renderSetParseMethod(SetShape s) {
-
+        writer
+                .openBlock("def self.parse(list)")
+                .openBlock("data = list.map do |value|")
+                .call(() -> {
+                    Shape memberTarget =
+                            model.expectShape(s.getMember().getTarget());
+                    memberTarget
+                            .accept(new MemberDeserializer(s.getMember(), "", "value", true));
+                })
+                .closeBlock("end")
+                .write("Set.new(data)")
+                .closeBlock("end");
     }
 
     @Override
     protected void renderListParseMethod(ListShape s) {
-
+        writer
+                .openBlock("def self.parse(list)")
+                .openBlock("list.map do |value|")
+                .call(() -> {
+                    Shape memberTarget =
+                            model.expectShape(s.getMember().getTarget());
+                    memberTarget
+                            .accept(new MemberDeserializer(s.getMember(),"", "value", !s.hasTrait(SparseTrait.class)));
+                })
+                .closeBlock("end")
+                .closeBlock("end");
     }
 
     @Override
     protected void renderStructureParseMethod(StructureShape s) {
-
+        writer
+                .openBlock("def self.parse(map)")
+                .write("data = Types::$L.new", symbolProvider.toSymbol(s).getName())
+                .call(() -> renderMemberParsers(s))
+                .write("return data")
+                .closeBlock("end");
     }
 
     @Override
     protected void renderOperationParseMethod(OperationShape operation, Shape outputShape) {
-
+        writer
+                .openBlock("def self.parse(http_resp)")
+                .write("data = Types::$L.new", symbolProvider.toSymbol(outputShape).getName())
+                .write("body = http_resp.body.read")
+                .write("return data if body.empty?")
+                .write("map = Hearth::JSON.load(body)");
+        renderMemberParsers(outputShape);
+        writer
+                .write("data")
+                .closeBlock("end");
     }
 
     @Override
-    protected void renderErrorParseMethod(Shape s) {
-
+    protected void renderErrorParseMethod(Shape shape) {
+        writer
+                .openBlock("def self.parse(http_resp)")
+                .write("data = Types::$L.new", symbolProvider.toSymbol(shape).getName())
+                .write("body = http_resp.body.read")
+                .write("return data if body.empty?")
+                .write("map = Hearth::JSON.load(body)");
+        renderMemberParsers(shape);
+        writer
+                .write("data")
+                .closeBlock("end");
     }
 
+    private class MemberDeserializer extends ShapeVisitor.Default<Void> {
+
+        private final String jsonGetter;
+        private final String dataSetter;
+        private final MemberShape memberShape;
+        private final boolean checkRequired;
+
+        MemberDeserializer(MemberShape memberShape,
+                           String dataSetter, String jsonGetter, boolean checkRequired) {
+            this.jsonGetter = jsonGetter;
+            this.dataSetter = dataSetter;
+            this.memberShape = memberShape;
+            this.checkRequired = checkRequired;
+        }
+
+        private String checkRequired() {
+            if (this.checkRequired) {
+                return " unless " + jsonGetter + ".nil?";
+            } else {
+                return "";
+            }
+        }
+
+        /**
+         * For simple shapes, just copy to the data.
+         */
+        @Override
+        protected Void getDefault(Shape shape) {
+            writer.write("$L$L$L", dataSetter, jsonGetter, checkRequired());
+            return null;
+        }
+
+        private void rubyFloat() {
+            writer.write("$LHearth::NumberHelper.deserialize($L)$L", dataSetter, jsonGetter, checkRequired());
+        }
+
+        @Override
+        public Void doubleShape(DoubleShape shape) {
+            rubyFloat();
+            return null;
+        }
+
+        @Override
+        public Void floatShape(FloatShape shape) {
+            rubyFloat();
+            return null;
+        }
+
+        @Override
+        public Void blobShape(BlobShape shape) {
+            writer.write("$1LBase64::decode64($2L) unless $2L.nil?", dataSetter, jsonGetter);
+            return null;
+        }
+
+        @Override
+        public Void timestampShape(TimestampShape shape) {
+            // the default protocol format is date_time, which is parsed by Time.parse
+            Optional<TimestampFormatTrait> format = memberShape.getTrait(TimestampFormatTrait.class);
+            if (format.isPresent()) {
+                switch (format.get().getFormat()) {
+                    case HTTP_DATE:
+                    case DATE_TIME:
+                        writer.write("$1LTime.parse($2L) if $2L", dataSetter, jsonGetter);
+                        break;
+                    case EPOCH_SECONDS:
+                    default:
+                        writer.write("$1LTime.at($2L.to_i) if $2L", dataSetter, jsonGetter);
+                        break;
+                }
+            } else {
+                writer.write("$1LTime.at($2L.to_i) if $2L", dataSetter, jsonGetter);
+            }
+            return null;
+        }
+
+        /**
+         * For complex shapes, simply delegate to their builder.
+         */
+        private void defaultComplexDeserializer(Shape shape) {
+            if (checkRequired) {
+                writer.write("$1LParsers::$2L.parse($3L) unless $3L.nil?",
+                        dataSetter, symbolProvider.toSymbol(shape).getName(),
+                        jsonGetter);
+            } else {
+                writer.write("$1L(Parsers::$2L.parse($3L) unless $3L.nil?)",
+                        dataSetter, symbolProvider.toSymbol(shape).getName(),
+                        jsonGetter);
+            }
+        }
+
+        @Override
+        public Void listShape(ListShape shape) {
+            defaultComplexDeserializer(shape);
+            return null;
+        }
+
+        @Override
+        public Void mapShape(MapShape shape) {
+            defaultComplexDeserializer(shape);
+            return null;
+        }
+
+        @Override
+        public Void structureShape(StructureShape shape) {
+            defaultComplexDeserializer(shape);
+            return null;
+        }
+
+        @Override
+        public Void unionShape(UnionShape shape) {
+            defaultComplexDeserializer(shape);
+            return null;
+        }
+    }
 }
