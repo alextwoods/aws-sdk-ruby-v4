70ad742d2091a5741af1115cf677eaa319d49e3e
Matt Muller <mamuller@amazon.com>
2022-03-16 12:59:36 -0700

    Working JSON 1.0 protocol

--- codegen/smithy-aws-ruby-codegen/src/main/java/software/amazon/smithy/aws/ruby/codegen/protocol/json10/generators/StubsGenerator.java
+++ codegen/smithy-aws-ruby-codegen/src/main/java/software/amazon/smithy/aws/ruby/codegen/protocol/json10/generators/StubsGenerator.java
@@ -15,15 +15,33 @@
 
 package software.amazon.smithy.aws.ruby.codegen.protocol.json10.generators;
 
+import java.util.Optional;
+import java.util.stream.Stream;
+import software.amazon.smithy.codegen.core.Symbol;
+import software.amazon.smithy.model.shapes.BlobShape;
+import software.amazon.smithy.model.shapes.DoubleShape;
+import software.amazon.smithy.model.shapes.FloatShape;
 import software.amazon.smithy.model.shapes.ListShape;
 import software.amazon.smithy.model.shapes.MapShape;
+import software.amazon.smithy.model.shapes.MemberShape;
 import software.amazon.smithy.model.shapes.OperationShape;
 import software.amazon.smithy.model.shapes.SetShape;
 import software.amazon.smithy.model.shapes.Shape;
+import software.amazon.smithy.model.shapes.ShapeVisitor;
 import software.amazon.smithy.model.shapes.StructureShape;
+import software.amazon.smithy.model.shapes.TimestampShape;
 import software.amazon.smithy.model.shapes.UnionShape;
+import software.amazon.smithy.model.traits.HttpHeaderTrait;
+import software.amazon.smithy.model.traits.HttpLabelTrait;
+import software.amazon.smithy.model.traits.HttpPayloadTrait;
+import software.amazon.smithy.model.traits.HttpQueryTrait;
+import software.amazon.smithy.model.traits.JsonNameTrait;
+import software.amazon.smithy.model.traits.SparseTrait;
+import software.amazon.smithy.model.traits.TimestampFormatTrait;
+import software.amazon.smithy.model.traits.XmlNamespaceTrait;
 import software.amazon.smithy.ruby.codegen.GenerationContext;
 import software.amazon.smithy.ruby.codegen.generators.StubsGeneratorBase;
+import software.amazon.smithy.ruby.codegen.trait.NoSerializeTrait;
 
 public class StubsGenerator extends StubsGeneratorBase {
 
@@ -33,32 +51,250 @@ public class StubsGenerator extends StubsGeneratorBase {
 
     @Override
     protected void renderUnionStubMethod(UnionShape shape) {
+        Symbol symbol = symbolProvider.toSymbol(shape);
+        writer
+                .openBlock("def self.stub(stub)")
+                .write("data = {}")
+                .write("case stub");
 
+        shape.members().forEach((member) -> {
+            writer
+                    .write("when Types::$L::$L", shape.getId().getName(), symbolProvider.toMemberName(member))
+                    .indent();
+            renderUnionMemberStubber(shape, member);
+            writer.dedent();
+        });
+        writer.openBlock("else")
+                .write("raise ArgumentError,\n\"Expected input to be one of the subclasses of Types::$L\"",
+                        symbol.getName())
+                .closeBlock("end")
+                .write("")
+                .write("data")
+                .closeBlock("end");
+    }
+
+    private void renderUnionMemberStubber(UnionShape shape, MemberShape member) {
+        Shape target = model.expectShape(member.getTarget());
+        String symbolName = "'" + member.getMemberName() + "'";
+        String dataSetter = "data[" + symbolName + "] = ";
+        target.accept(new MemberSerializer(member, dataSetter, "stub.__getobj__", false));
     }
 
     @Override
     protected void renderListStubMethod(ListShape shape) {
-
+        writer
+                .openBlock("def self.stub(stub)")
+                .write("stub ||= []")
+                .write("data = []")
+                .openBlock("stub.each do |element|")
+                .call(() -> {
+                    Shape memberTarget =
+                            model.expectShape(shape.getMember().getTarget());
+                    memberTarget
+                            .accept(new MemberSerializer(shape.getMember(),"data << ", "element", !shape.hasTrait(SparseTrait.class)));
+                })
+                .closeBlock("end")
+                .write("data")
+                .closeBlock("end");
     }
 
     @Override
     protected void renderSetStubMethod(SetShape shape) {
-
+        writer
+                .openBlock("def self.stub(stub)")
+                .write("stub ||= []")
+                .write("data = []")
+                .openBlock("stub.each do |element|")
+                .call(() -> {
+                    Shape memberTarget =
+                            model.expectShape(shape.getMember().getTarget());
+                    memberTarget
+                            .accept(new MemberSerializer(shape.getMember(),"data << ", "element", true));
+                })
+                .closeBlock("end")
+                .write("data")
+                .closeBlock("end");
     }
 
     @Override
     protected void renderMapStubMethod(MapShape shape) {
-
+        writer
+                .openBlock("def self.stub(stub)")
+                .write("stub ||= {}")
+                .write("data = {}")
+                .openBlock("stub.each do |key, value|")
+                .call(() -> {
+                    Shape valueTarget = model.expectShape(shape.getValue().getTarget());
+                    valueTarget
+                            .accept(new MemberSerializer(shape.getValue(), "data[key] = ", "value", !shape.hasTrait(SparseTrait.class)));
+                })
+                .closeBlock("end")
+                .write("data")
+                .closeBlock("end");
     }
 
     @Override
     protected void renderStructureStubMethod(StructureShape shape) {
-
+        String typeName = symbolProvider.toSymbol(shape).getName();
+        writer
+                .openBlock("def self.stub(stub)")
+                .write("stub ||= Types::$L.new", typeName)
+                .write("data = {}")
+                .call(() -> renderMemberStubbers(shape))
+                .write("data")
+                .closeBlock("end");
     }
 
     @Override
     protected void renderOperationStubMethod(OperationShape operation, Shape outputShape) {
+        writer
+                .openBlock("def self.stub(http_resp, stub:)")
+                .write("data = {}")
+                .call(() -> renderMemberStubbers(outputShape))
+                .write("http_resp.body = StringIO.new(Hearth::JSON.dump(data))")
+                .closeBlock("end");
+    }
 
+    private void renderMemberStubbers(Shape s) {
+        //remove members w/ http traits or marked NoSerialize
+        Stream<MemberShape> serializeMembers = s.members().stream()
+                .filter(NoSerializeTrait.excludeNoSerializeMembers());
+
+        serializeMembers.forEach((member) -> {
+            Shape target = model.expectShape(member.getTarget());
+
+            String symbolName = ":" + symbolProvider.toMemberName(member);
+            String dataName = "'" + member.getMemberName() + "'";
+            if (member.hasTrait(JsonNameTrait.class)) {
+                dataName = "'" + member.expectTrait(JsonNameTrait.class).getValue() + "'";
+            }
+            String dataSetter = "data[" + dataName + "] = ";
+            String inputGetter = "stub[" + symbolName + "]";
+            target.accept(new MemberSerializer(member, dataSetter, inputGetter, true));
+        });
+    }
+
+    private class MemberSerializer extends ShapeVisitor.Default<Void> {
+
+        private final MemberShape memberShape;
+        private final String inputGetter;
+        private final String dataSetter;
+        private final boolean checkRequired;
+
+
+        MemberSerializer(MemberShape memberShape,
+                         String dataSetter, String inputGetter, boolean checkRequired) {
+            this.memberShape = memberShape;
+            this.inputGetter = inputGetter;
+            this.dataSetter = dataSetter;
+            this.checkRequired = checkRequired;
+        }
+
+        private String checkRequired() {
+            if (this.checkRequired) {
+                return " unless " + inputGetter + ".nil?";
+            } else {
+                return "";
+            }
+        }
+
+        @Override
+        protected Void getDefault(Shape shape) {
+            writer.write("$L$L$L", dataSetter, inputGetter, checkRequired());
+            return null;
+        }
+
+        private void rubyFloat() {
+            writer.write("$LHearth::NumberHelper.serialize($L)", dataSetter, inputGetter);
+        }
+
+        @Override
+        public Void doubleShape(DoubleShape shape) {
+            rubyFloat();
+            return null;
+        }
+
+        @Override
+        public Void floatShape(FloatShape shape) {
+            rubyFloat();
+            return null;
+        }
+
+        @Override
+        public Void blobShape(BlobShape shape) {
+            writer.write("$LBase64::encode64($L)$L", dataSetter, inputGetter, checkRequired());
+            return null;
+        }
+
+        @Override
+        public Void timestampShape(TimestampShape shape) {
+            // the default protocol format is date_time
+            Optional<TimestampFormatTrait> format = memberShape.getTrait(TimestampFormatTrait.class);
+            if (format.isPresent()) {
+                switch (format.get().getFormat()) {
+                    case EPOCH_SECONDS:
+                        writer.write("$LHearth::TimeHelper.to_epoch_seconds($L)$L", dataSetter, inputGetter,
+                                checkRequired());
+                        break;
+                    case HTTP_DATE:
+                        writer.write("$LHearth::TimeHelper.to_http_date($L)$L", dataSetter, inputGetter,
+                                checkRequired());
+                        break;
+                    case DATE_TIME:
+                    default:
+                        writer.write("$LHearth::TimeHelper.to_date_time($L)$L", dataSetter, inputGetter,
+                                checkRequired());
+                        break;
+                }
+            } else {
+                writer.write("$LHearth::TimeHelper.to_epoch_seconds($L)$L", dataSetter, inputGetter,
+                        checkRequired());
+            }
+            return null;
+        }
+
+        /**
+         * For complex shapes, simply delegate to their Stubber.
+         */
+        private void defaultComplexSerializer(Shape shape) {
+            if (checkRequired) {
+                writer.write("$1LStubs::$2L.stub($3L) unless $3L.nil?", dataSetter,
+                        symbolProvider.toSymbol(shape).getName(), inputGetter);
+            } else {
+                writer.write("$1L(Stubs::$2L.stub($3L) unless $3L.nil?)", dataSetter,
+                        symbolProvider.toSymbol(shape).getName(), inputGetter);
+            }
+        }
+
+        @Override
+        public Void listShape(ListShape shape) {
+            defaultComplexSerializer(shape);
+            return null;
+        }
+
+        @Override
+        public Void setShape(SetShape shape) {
+            defaultComplexSerializer(shape);
+            return null;
+        }
+
+        @Override
+        public Void mapShape(MapShape shape) {
+            defaultComplexSerializer(shape);
+            return null;
+        }
+
+        @Override
+        public Void structureShape(StructureShape shape) {
+            defaultComplexSerializer(shape);
+            return null;
+        }
+
+        @Override
+        public Void unionShape(UnionShape shape) {
+            defaultComplexSerializer(shape);
+            return null;
+        }
     }
 }
 
