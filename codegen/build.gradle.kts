9c1ff01fe44f112db74cf6e3ff42334e112659fa
Alex Woods <alexwoo@amazon.com>
2022-03-29 16:46:01 -0700

    Upgrade gradle

--- codegen/build.gradle.kts
+++ codegen/build.gradle.kts
@@ -27,7 +27,7 @@ allprojects {
     }
 }
 
-extra["smithyVersion"] = "[1.18.0,2.0.0["
+extra["smithyVersion"] = "[1.19.0,2.0.0["
 
 // The root project doesn't produce a JAR.
 tasks["jar"].enabled = false
