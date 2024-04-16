fb1b59d0a773113a43ec18e0a29f43355e72a837
Alex Woods <alexwoo@amazon.com>
2024-04-16 15:58:44 -0700

    Remove signer option from sigv4 auth scheme

--- gems/aws-sdk-core/lib/aws-sdk-core/auth_schemes/sigv4.rb
+++ gems/aws-sdk-core/lib/aws-sdk-core/auth_schemes/sigv4.rb
@@ -4,10 +4,10 @@ module AWS::SDK::Core
   module AuthSchemes
     # SigV4 authentication scheme.
     class SigV4 < Hearth::AuthSchemes::Base
-      def initialize(signer: Signers::SigV4.new)
+      def initialize()
         super(
           scheme_id: 'aws.auth#sigv4',
-          signer: signer,
+          signer: Signers::SigV4.new,
           identity_type: Identities::SigV4
         )
       end
