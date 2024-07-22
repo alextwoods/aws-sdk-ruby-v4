01f7004ca3d744118802d66743ff716e2e3a59d4
Matt Muller <mamuller@amazon.com>
2024-07-22 10:52:07 -0400

    Reformat and checkstyle extra files

--- codegen/smithy-aws-ruby-codegen/src/main/java/software/amazon/smithy/aws/ruby/codegen/customizations/S3ControlCustomizations.java
+++ codegen/smithy-aws-ruby-codegen/src/main/java/software/amazon/smithy/aws/ruby/codegen/customizations/S3ControlCustomizations.java
@@ -1,5 +1,21 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 package software.amazon.smithy.aws.ruby.codegen.customizations;
 
+import java.util.List;
 import software.amazon.smithy.model.Model;
 import software.amazon.smithy.model.shapes.ServiceShape;
 import software.amazon.smithy.model.shapes.ShapeId;
@@ -8,8 +24,6 @@ import software.amazon.smithy.ruby.codegen.config.ClientConfig;
 import software.amazon.smithy.ruby.codegen.config.ConfigProviderChain;
 import software.amazon.smithy.ruby.codegen.rulesengine.BuiltInBinding;
 import software.amazon.smithy.rulesengine.aws.language.functions.AwsBuiltIns;
-
-import java.util.List;
 
 public class S3ControlCustomizations implements RubyIntegration {
 
@@ -21,8 +35,8 @@ public class S3ControlCustomizations implements RubyIntegration {
     @Override
     public List<BuiltInBinding> builtInBindings() {
         return List.of(BuiltInBinding.builder()
-                        .builtIn(AwsBuiltIns.S3_CONTROL_USE_ARN_REGION)
-                        .fromConfig(USE_ARN_REGION)
+                .builtIn(AwsBuiltIns.S3_CONTROL_USE_ARN_REGION)
+                .fromConfig(USE_ARN_REGION)
                 .build());
     }
 
--- codegen/smithy-aws-ruby-codegen/src/main/java/software/amazon/smithy/aws/ruby/codegen/protocol/ec2/Ec2Query.java
+++ codegen/smithy-aws-ruby-codegen/src/main/java/software/amazon/smithy/aws/ruby/codegen/protocol/ec2/Ec2Query.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
 package software.amazon.smithy.aws.ruby.codegen.protocol.ec2;
 
 import java.util.logging.Logger;
@@ -5,7 +20,6 @@ import software.amazon.smithy.aws.ruby.codegen.protocol.ec2.generators.BuilderGe
 import software.amazon.smithy.aws.ruby.codegen.protocol.ec2.generators.ErrorsGenerator;
 import software.amazon.smithy.aws.ruby.codegen.protocol.ec2.generators.ParserGenerator;
 import software.amazon.smithy.aws.ruby.codegen.protocol.ec2.generators.StubsGenerator;
-import software.amazon.smithy.aws.traits.protocols.AwsJson1_1Trait;
 import software.amazon.smithy.aws.traits.protocols.Ec2QueryTrait;
 import software.amazon.smithy.model.shapes.ShapeId;
 import software.amazon.smithy.ruby.codegen.ApplicationTransport;
