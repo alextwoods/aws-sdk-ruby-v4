c8619686bc7ea1730c359f094ae4cafae3fb06b0
Juli Tera <terajul@amazon.com>
2024-08-23 11:10:33 -0700

    Update fetching of current_span

--- codegen/smithy-aws-ruby-codegen/src/main/resources/ruby/middleware/request_id.rb
+++ codegen/smithy-aws-ruby-codegen/src/main/resources/ruby/middleware/request_id.rb
@@ -11,7 +11,7 @@ module Middleware
       output = @app.call(input, context)
       request_id = context.response.headers['x-amz-request-id']
       output.metadata[:request_id] = request_id
-      current_span = context.config.telemetry_provider.context_manager.current_span
+      current_span = context.tracer.current_span
       current_span.set_attribute('aws.request_id', request_id) if request_id
       output
     end
