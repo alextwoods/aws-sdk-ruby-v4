712d741b9b7e8b52ea2efa2f8bc33343c444045d
Juli Tera <terajul@amazon.com>
2024-08-23 11:13:50 -0700

    Update projections

--- gems/aws-sdk-cloudwatch/lib/aws-sdk-cloudwatch/middleware/request_id.rb
+++ gems/aws-sdk-cloudwatch/lib/aws-sdk-cloudwatch/middleware/request_id.rb
@@ -21,7 +21,7 @@ module AWS::SDK::CloudWatch
         output = @app.call(input, context)
         request_id = context.response.headers['x-amz-request-id']
         output.metadata[:request_id] = request_id
-        current_span = context.config.telemetry_provider.context_manager.current_span
+        current_span = context.tracer.current_span
         current_span.set_attribute('aws.request_id', request_id) if request_id
         output
       end
